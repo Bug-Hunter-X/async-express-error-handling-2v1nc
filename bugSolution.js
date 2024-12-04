3f17462c4c68eba48bf022fcdbfdc1994afd0c1c
BugHunter-X <askzenai@gmail.com>
2024-12-04 06:58:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  setTimeout(() => {
+    try {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        res.send('Success!');
+      } else {
+        throw new Error('Something went wrong!');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  }, 1000);
+});
+app.listen(3000, () => console.log('Server listening on port 3000'));
